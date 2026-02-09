2785788ba878252ab83cca04e64ff41364976888
ChristianGabbert2640 <christian.gabbert40@unach.mx>
2026-02-08 20:21:01 -0600

    Agregar archivos de proyecto.

---
+++ 3NLIDTS-ChristianGabbert-03.1-Csharp/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace _3NLIDTS_ChristianGabbert_03._1_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKelvin = new System.Windows.Forms.TextBox();
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.rbKelvin = new System.Windows.Forms.RadioButton();
+            this.rbFahrenheit = new System.Windows.Forms.RadioButton();
+            this.rbCelsius = new System.Windows.Forms.RadioButton();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(19, 269);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(153, 51);
+            this.btnLimpiar.TabIndex = 19;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Kelvin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Fahrenheit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Celsius";
+            // 
+            // txtKelvin
+            // 
+            this.txtKelvin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKelvin.Location = new System.Drawing.Point(162, 206);
+            this.txtKelvin.Multiline = true;
+            this.txtKelvin.Name = "txtKelvin";
+            this.txtKelvin.Size = new System.Drawing.Size(201, 41);
+            this.txtKelvin.TabIndex = 15;
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFahrenheit.Location = new System.Drawing.Point(162, 154);
+            this.txtFahrenheit.Multiline = true;
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(201, 41);
+            this.txtFahrenheit.TabIndex = 14;
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCelsius.Location = new System.Drawing.Point(162, 112);
+            this.txtCelsius.Multiline = true;
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(201, 41);
+            this.txtCelsius.TabIndex = 13;
+            // 
+            // rbKelvin
+            // 
+            this.rbKelvin.AutoSize = true;
+            this.rbKelvin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbKelvin.Location = new System.Drawing.Point(289, 55);
+            this.rbKelvin.Name = "rbKelvin";
+            this.rbKelvin.Size = new System.Drawing.Size(74, 24);
+            this.rbKelvin.TabIndex = 12;
+            this.rbKelvin.TabStop = true;
+            this.rbKelvin.Text = "Kelvin";
+            this.rbKelvin.UseVisualStyleBackColor = true;
+            this.rbKelvin.CheckedChanged += new System.EventHandler(this.rbKelvin_CheckedChanged);
+            // 
+            // rbFahrenheit
+            // 
+            this.rbFahrenheit.AutoSize = true;
+            this.rbFahrenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFahrenheit.Location = new System.Drawing.Point(148, 55);
+            this.rbFahrenheit.Name = "rbFahrenheit";
+            this.rbFahrenheit.Size = new System.Drawing.Size(114, 24);
+            this.rbFahrenheit.TabIndex = 11;
+            this.rbFahrenheit.TabStop = true;
+            this.rbFahrenheit.Text = "Fahrenheit";
+            this.rbFahrenheit.UseVisualStyleBackColor = true;
+            this.rbFahrenheit.CheckedChanged += new System.EventHandler(this.rbFahrenheit_CheckedChanged);
+            // 
+            // rbCelsius
+            // 
+            this.rbCelsius.AutoSize = true;
+            this.rbCelsius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbCelsius.Location = new System.Drawing.Point(31, 55);
+            this.rbCelsius.Name = "rbCelsius";
+            this.rbCelsius.Size = new System.Drawing.Size(85, 24);
+            this.rbCelsius.TabIndex = 10;
+            this.rbCelsius.TabStop = true;
+            this.rbCelsius.Text = "Celsius";
+            this.rbCelsius.UseVisualStyleBackColor = true;
+            this.rbCelsius.CheckedChanged += new System.EventHandler(this.rbCelsius_CheckedChanged);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(236, 269);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(153, 51);
+            this.btnCalcular.TabIndex = 20;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKelvin);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.rbKelvin);
+            this.Controls.Add(this.rbFahrenheit);
+            this.Controls.Add(this.rbCelsius);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKelvin;
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.RadioButton rbKelvin;
+        private System.Windows.Forms.RadioButton rbFahrenheit;
+        private System.Windows.Forms.RadioButton rbCelsius;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ 3NLIDTS-ChristianGabbert-03.1-Csharp/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3NLIDTS_ChristianGabbert_03._1_Csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rbCelsius.Checked)
+                {
+                    double c = double.Parse(txtCelsius.Text);
+                    txtFahrenheit.Text = (c * 9 / 5 + 32).ToString("0.00");
+                    txtKelvin.Text = (c + 273.15).ToString("0.00");
+                }
+                else if (rbFahrenheit.Checked)
+                {
+                    double f = double.Parse(txtFahrenheit.Text);
+                    txtCelsius.Text = ((f - 32) * 5 / 9).ToString("0.00");
+                    txtKelvin.Text = (((f - 32) * 5 / 9) + 273.15).ToString("0.00");
+                }
+                else if (rbKelvin.Checked)
+                {
+                    double k = double.Parse(txtKelvin.Text);
+                    txtCelsius.Text = (k - 273.15).ToString("0.00");
+                    txtFahrenheit.Text = ((k - 273.15) * 9 / 5 + 32).ToString("0.00");
+                }
+                else
+                {
+                    MessageBox.Show("Selecciona un tipo de temperatura");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ingresa un valor numerico valido");
+            }
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            EstadoInicial();
+
+        }
+
+        private void rbKelvin_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbKelvin.Checked)
+            {
+                txtKelvin.Enabled = true;
+                txtCelsius.Enabled = false;
+                txtFahrenheit.Enabled = false;
+
+                txtCelsius.Text = "";
+                txtFahrenheit.Text = "";
+            }
+        }
+
+        private void rbFahrenheit_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbFahrenheit.Checked)
+            {
+                txtFahrenheit.Enabled = true;
+                txtCelsius.Enabled = false;
+                txtKelvin.Enabled = false;
+
+                txtCelsius.Text = "";
+                txtKelvin.Text = "";
+            }
+        }
+
+        private void rbCelsius_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbCelsius.Checked)
+            {
+                txtCelsius.Enabled = true;
+                txtFahrenheit.Enabled = false;
+                txtKelvin.Enabled = false;
+
+                txtFahrenheit.Text = "";
+                txtKelvin.Text = "";
+            }
+        }
+
+        void EstadoInicial()
+        {
+            rbCelsius.Checked = false;
+            rbFahrenheit.Checked = false;
+            rbKelvin.Checked = false;
+
+            txtCelsius.Text = "";
+            txtFahrenheit.Text = "";
+            txtKelvin.Text = "";
+
+            txtCelsius.Enabled = false;
+            txtFahrenheit.Enabled = false;
+            txtKelvin.Enabled = false;
+        }
+    }
+}
